ae94a5a61d4c9843ecaa22227531c62609fe7865
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-12 19:33:31 +0700

    update

--- client/src/pages/ComplainAdmin.js
+++ client/src/pages/ComplainAdmin.js
@@ -1,77 +1,127 @@
 // import hook
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react';
 
-import NavbarAdmin from '../components/NavbarAdmin'
+import NavbarAdmin from '../components/NavbarAdmin';
 
-import { Container, Row, Col } from 'react-bootstrap'
-import Contact from '../components/complain/Contact'
+import { Container, Row, Col } from 'react-bootstrap';
+import Contact from '../components/complain/Contact';
+import Chat from '../components/complain/Chat';
 
-// import here
+import { UserContext } from '../context/userContext';
 
-// import socket.io-client 
-import {io} from 'socket.io-client'
+// import socket.io-client
+import { io } from 'socket.io-client';
 
 // initial variable outside socket
-let socket
+let socket;
 export default function ComplainAdmin() {
-    const [contact, setContact] = useState(null)
-    const [contacts, setContacts] = useState([])
-    // code here
+  const [contact, setContact] = useState(null);
+  const [contacts, setContacts] = useState([]);
 
-    const title = "Complain admin"
-    document.title = 'DumbMerch | ' + title
+  const [messages, setMessages] = useState([]);
 
-    // code here
+  const title = 'Complain admin';
+  document.title = 'DumbMerch | ' + title;
 
-    useEffect(() =>{
-        socket = io('http://localhost:5000', {
-            auth: {
-                token: localStorage.getItem('token')
-            },
-            // code here
-        })
+  const [state] = useContext(UserContext);
 
-        // code here
+  useEffect(() => {
+    socket = io('http://localhost:5000', {
+      auth: {
+        token: localStorage.getItem('token'),
+      },
+    });
 
-        loadContacts()
-        // code here
+    socket.on('new message', () => {
+      socket.emit('load messages', contact?.id);
+    });
 
-        return () => {
-            socket.disconnect()
-        }
-    }, []) // code here
+    loadContacts();
+    loadMessages();
 
-    const loadContacts = () => {
-        socket.emit("load customer contacts")
-        socket.on("customer contacts", (data) => {
-            // filter just customers which have sent a message
-            let dataContacts = // code here
-            
-            // manipulate customers to add message property with the newest message
-            // code here
-            setContacts(dataContacts)
-        })
+    return () => {
+      socket.disconnect();
+    };
+  }, [messages]); // code here
+
+  const loadContacts = () => {
+    socket.emit('load customer contacts');
+    socket.on('customer contacts', (data) => {
+      // filter just customers which have sent a message
+      let dataContacts = data.map((item) => ({
+        ...item,
+        message:
+          item.senderMessage.length > 0
+            ? item.senderMessage[item.senderMessage.length - 1].message
+            : 'Click here to start message',
+      }));
+      setContacts(dataContacts);
+    });
+  };
+
+  // used for active style when click contact
+  const onClickContact = (data) => {
+    setContact(data);
+    socket.emit('load messages', data.id);
+  };
+
+  const loadMessages = () => {
+    socket.on('messages', (data) => {
+      console.log(data);
+      if (data.length > 0) {
+        const dataMessages = data.map((item) => ({
+          idSender: item.sender.id,
+          message: item.message,
+        }));
+        setMessages(dataMessages);
+      } else {
+        setMessages([]);
+      }
+    });
+  };
+
+  const onSendMessage = (e) => {
+    if (e.key === 'Enter') {
+      const data = {
+        idRecipient: contact.id,
+        message: e.target.value,
+      };
+
+      socket.emit('send message', data);
+      e.target.value = '';
     }
+  };
 
-    // used for active style when click contact
-    const onClickContact = (data) => {
-        setContact(data)
-        // code here
-    }
-
-    // code here
-
-    return (
-        <>
-            <NavbarAdmin title={title} />
-            <Container fluid style={{height: '89.5vh'}}>
-                <Row>
-                    <Col md={3} style={{height: '89.5vh'}} className="px-3 border-end border-dark overflow-auto">
-                        <Contact dataContact={contacts} clickContact={onClickContact} contact={contact}/>
-                    </Col>
-                    {/* code here */}
-                </Row>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <NavbarAdmin title={title} />
+      <Container fluid style={{ height: '89.5vh' }}>
+        <Row>
+          <Col
+            md={3}
+            style={{ height: '89.5vh' }}
+            className="px-3 border-end border-dark overflow-auto"
+          >
+            <Contact
+              dataContact={contacts}
+              clickContact={onClickContact}
+              contact={contact}
+            />
+          </Col>
+          <Col
+            md={9}
+            style={{ height: '89.5vh' }}
+            className="px-3 border-end border-dark overflow-auto"
+          >
+            <Chat
+              contact={contact}
+              messages={messages}
+              user={state.user}
+              sendMessage={onSendMessage}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
